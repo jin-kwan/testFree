9c4a3dc531c4597f8ab85f8b3a180466394fb64d
JK <speedjjk@naver.com>
2019-08-25 18:11:43 +0900

    first

---
+++ gitTest/Assets/ZProjects/Scripts/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class App : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		String a = "App";
+		Debug.Log("log" +  a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
